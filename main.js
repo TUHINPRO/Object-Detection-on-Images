7603015a5e8e7147e240d190987f7e936c1edb20
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-06-06 12:15:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 img="";
-
+status="";
 function preload() {
     img=loadImage('dog_cat.jpg');
 }
@@ -7,7 +7,25 @@ function preload() {
 function setup() {
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocosd',modelLoaded);
+    
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
 }
+
+function modelLoaded() {
+    console.log("Model is loaded :)");
+    status=true;
+    objectDetector.detect(img,gotResult);
+    
+}
+function gotResult(error,results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+} 
 
 function draw() {
     image(img,0,0,640,420);
@@ -16,5 +34,11 @@ function draw() {
     noFill();
     stroke('#FF0000');
     rect(5,20,300,300);
+
+    fill('#FF0000');
+    text('cat',310,90);
+    noFill();
+    stroke('#FF0000')
+    rect(300,80,300,300);
     
 }
